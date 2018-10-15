00580d9c1fd7340db0e5c5950384bef2bf4039a3
Nero_Sen <niravsen11@gmail.com>
2018-10-14 21:24:31 -0400

    db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name) VALUES ("Beef Burger");
+INSERT INTO burgers (burger_name) VALUES ("Deep Fried Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Vegan Burger");
